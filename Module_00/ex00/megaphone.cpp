56343c7d1fa1ca10c453e48c70f80a75ea4cd010
Elvira Martínez López <elviramarlo23@gmail.com>
2022-03-21 22:55:28 +0100

    ex00

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 22:13:02 by elvmarti          #+#    #+#             */
-/*   Updated: 2022/03/21 22:37:44 by elvmarti         ###   ########.fr       */
+/*   Updated: 2022/03/21 22:53:05 by elvmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,19 @@ int	main(int argc, char **argv)
 {
 	int		i;
 	int		x;
-	char	c;
 	
-	i = 1;
+	i = 0;
 	if (argc > 1)
 	{
-		while (argv[i])
+		while (argv[++i])
 		{
 			x = 0;
 			while (argv[i][x])
-			{
-				c = toupper(argv[i][x]);
-				std::cout << c;
-				x++;
-			}
-			i++;
+				std::cout << (char)toupper(argv[i][x++]);
 		}
 		std::cout << std::endl;
 	}
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
 }
