28c8cc22579883380c8b4742c0e3499e80714c3e
Elvira Martinez lopez <elvmarti@c3r13s5.42madrid.com>
2022-05-19 20:35:39 +0200

    Module 07

---
+++ Module_07/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elvmarti <elvmarti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/19 17:44:13 by elvmarti          #+#    #+#             */
+/*   Updated: 2022/05/19 18:01:55 by elvmarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp;
+
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template<typename T>
+T const &min(T &a, T &b)
+{
+	return a < b ? a : b;
+}
+
+template<typename T>
+T const &max(T &a, T &b)
+{
+	return a > b ? a : b;
+}
